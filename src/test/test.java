db959443dd822ee7a5e166a8e61715446e7edff6
ljm <ljm3307@163.com>
2020-12-24 23:03:52 +0800

    'ljm'

---
+++ src/test/test.java
@@ -0,0 +1,44 @@
+package test;
+
+//import analyser.Analyser;
+//import error.CompileError;
+//import error.TokenizeError;
+//import instruction.Instruction;
+//import org.junit.Test;
+import error.TokenizeError;
+import org.junit.Test;
+import tokenizer.StringIter;
+import tokenizer.Token;
+import tokenizer.TokenType;
+import tokenizer.Tokenizer;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+public class test {
+    @Test
+    public void tokenizer() throws IOException, TokenizeError {
+        File file = new File("src/test/whileIns.txt");
+        Scanner sc = new Scanner(file);
+        StringIter it = new StringIter(sc);
+        Tokenizer tokenizer = new Tokenizer(it);
+        while(true) {
+            Token token = tokenizer.nextToken();
+            if(token.getTokenType() == TokenType.EOF)
+                break;
+            System.out.println(token.getValueString());
+            System.out.println(token.toString());
+        }
+    }
+
+//    @Test
+//    public void simpleCompile() throws IOException, CompileError {
+//        File file = new File("src/test/java/test/whileIns.txt");
+//        Scanner sc = new Scanner(file);
+//        StringIter it = new StringIter(sc);
+//        Tokenizer tokenizer = new Tokenizer(it);
+//        Analyser analyser = new Analyser(tokenizer);
+//        analyser.analyse("src/test/java/test/result.txt");
+//    }
+}
